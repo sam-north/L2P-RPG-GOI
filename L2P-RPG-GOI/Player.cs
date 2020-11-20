1bccdd32c818a862b51996194e8767598aff0134
Sam North <sam.north@fiscalnote.com>
2020-11-19 22:30:02 -0600

    -First Commit.  Created a player -Made a prompt that can prompt anything for the rest of the game -Made a print method.

---
+++ L2P-RPG-GOI/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace L2P_RPG_GOI
+{
+    public class Player
+    {
+        public Player()
+        {
+            Health = 100;
+            ExperiencePoints = 0;
+            Level = 1;
+
+            var random = new Random();
+            Strength = random.Next(0, 101);
+            Dexterity = random.Next(0, 101);
+            Intellect = random.Next(0, 101);
+        }
+
+        public string Name { get; set; }
+        public string Class { get; set; }
+
+        public int Health { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intellect { get; set; }
+    }
+}
---
+++ L2P-RPG-GOI/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace L2P_RPG_GOI
+{
+    class Program
+    {
+        static string WorldName = "Ghrothban";
+        static void Main(string[] args)
+        {
+            //welcome (cool ascii art)
+            Welcome();
+            //menu
+            Menu();
+                //create a character
+        }
+
+        private static void Welcome()
+        {
+            Print("Welcome to the game bitch.");
+            Print("///////*******////////");
+            Print("///////****///////////");
+            Print("///////*///***////////");
+            Print("///////*******////////");
+            Print("///////*-----*////////");
+            Print("///////*******////////");
+        }
+
+        private static void Menu()
+        {
+            var player = new Player();
+            player.Name = Prompt("What is your name?");
+            player.Class = Prompt("What class are you?  (Warrior, Archer, Mage)");
+
+            Print($"Welcome to {WorldName} fellow {player.Name}");
+            Print($"We have not had a {player.Class} at {WorldName} since the year 2019. Before the storm...of covid");
+            Print($"Oh MY!  Look at how strong you are! ******** Strength: {player.Strength}.");
+            Print($"Little dexterious dick bro are you! ******** Dexterity: {player.Dexterity}.");
+            Print($"You are sooooo smart!........       ******** Intellect: {player.Intellect}.");
+
+            Print("");
+            Print($"Congratulations.  You have created your character.  Enjoy the world of {WorldName}.");
+            Print($"Health: {player.Health}, Experience: {player.ExperiencePoints}, Level: {player.Level}");
+        }
+
+        private static string Prompt(string promptThis)
+        {
+            string input = "";
+            //check to see if they put anything in
+            while (input.Trim().Length <= 0)
+            {
+                //tell them to put something in.
+                Print(promptThis);
+                //let them input something
+                input = Console.ReadLine();
+            }
+            //return the something
+            return input;
+        }
+
+        private static void Print(string line)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
