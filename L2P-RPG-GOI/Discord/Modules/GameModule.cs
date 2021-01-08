0ef7c3e76c808a64841d4079a565e086f313bb99
Sam North <sam.north@fiscalnote.com>
2021-01-07 22:54:14 -0600

    -added grouping on our module.

--- L2P-RPG-GOI/Discord/Modules/GameModule.cs
+++ L2P-RPG-GOI/Discord/Modules/GameModule.cs
@@ -7,10 +7,11 @@ namespace L2P_RPG_GOI.Discord.Modules
 {
     // Keep in mind your module **must** be public and inherit ModuleBase.
     // If it isn't, it will not be discovered by AddModulesAsync!
+    [Group("rpg")]
     public class GameModule : ModuleBase<SocketCommandContext>
     {
         // ~say hello world -> hello world
-        [Command("RPGWelcome")]
+        [Command("welcome")]
         [Summary("Echoes a message.")]
         public Task Welcome()
         {
